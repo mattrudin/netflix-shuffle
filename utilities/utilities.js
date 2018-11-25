841d694ed6c82b681ee3cda0fa134ec5fd44a9d9
mattrudin <matthias.rudin@live.com>
2018-11-25 18:34:23 +0100

    Updated getGenreTags method

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,15 +1,12 @@
-import unirest from 'unirest'
-import { key } from '../.env_key'
-
 const getRelevantGenreID = array => (
     array.filter((item, index) => index <= 18 ? item : false)
 )
 
 const extractGenreTag = array => (
-    Object.keys(array).map(genre => genre.slice(4,-1))
+    array.map(genre => Object.keys(genre)).toString().split(",").map(genre => genre.slice(4))
 )
 
 export function getGenreTags(array) {
-    const relevantGenreID =  getRelevantGenreID(array)
+    const relevantGenreID = getRelevantGenreID(array)
     return extractGenreTag(relevantGenreID)
 }
